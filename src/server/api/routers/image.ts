17316fa7b2f189e4c9f9077b322123f55933856a
AxelReid <mr.webdevsemail@gmail.com>
2023-02-24 11:43:30 +0500

    commenting images

--- src/server/api/routers/image.ts
+++ src/server/api/routers/image.ts
@@ -1,10 +1,10 @@
-import { imgBase } from "@static/index";
-import { readdirSync } from "fs";
-import { dest } from "@components/upload";
+// import { imgBase } from "@static/index";
+// import { readdirSync } from "fs";
+// import { dest } from "@components/upload";
 import { createTRPCRouter, publicProcedure } from "../trpc";
 
 export const imagesRouter = createTRPCRouter({
   getAll: publicProcedure.query(
-    () => readdirSync(dest).map((name) => imgBase + name) || []
+    () => /* readdirSync(dest).map((name) => imgBase + name) ||*/ []
   ),
 });
